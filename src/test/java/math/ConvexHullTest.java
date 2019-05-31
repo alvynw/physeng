68dbfe9f7e28efcb70f8eb87b1b48009208deeb9
Alvyn Wang <alvyn.wang@gmail.com>
2019-05-30 22:13:49 -0700

    COM Unit Tests (#9)
    * test
    
    * test
    
    * test2
    
    * simpleTest

--- src/test/java/math/ConvexHullTest.java
+++ src/test/java/math/ConvexHullTest.java
@@ -74,4 +74,129 @@ public class ConvexHullTest {
 
         Path2D path = ConvexHull.getHull(vertices);
     }
+
+    @Test
+    public void doubleArrPoints()
+    {
+        double[] xpoints = {1.0, 0.0, 2.0, 0.0, 2.0};
+        double[] ypoints = {1.0, 0.0, 2.0, 2.0, 0.0};
+
+        int npoints = xpoints.length;
+        Path2D path = ConvexHull.getHull(xpoints, ypoints, npoints);
+        Vector2D[] vertices = pathVertices(path);
+
+        Vector2D[] expected = {new Vector2D(0, 0),
+                new Vector2D(2, 0),
+                new Vector2D(2, 2),
+                new Vector2D(0, 2)};
+
+        for (int i = 0; i < expected.length; i++) {
+            assertVector2DEquals(vertices[i], expected[i], TOLERANCE);
+        }
+    }
+
+    @Test
+    public void vectorTest()
+    {
+
+        Vector2D[] vector = {new Vector2D(1, 1),
+                new Vector2D(0, 0),
+                new Vector2D(2, 2),
+                new Vector2D(0, 2),
+                new Vector2D(2, 0)};
+
+        Path2D path = ConvexHull.getHull(vector);
+        Vector2D[] vertices = pathVertices(path);
+
+        Vector2D[] expected = {new Vector2D(0, 0),
+                new Vector2D(2, 0),
+                new Vector2D(2, 2),
+                new Vector2D(0, 2)};
+
+        for (int i = 0; i < expected.length; i++) {
+            assertVector2DEquals(vertices[i], expected[i], TOLERANCE);
+        }
+
+    }
+
+    @Test
+    public void intArrPoints()
+    {
+        double[] xpoints = {1, 0, 2, 0, 2};
+        double[] ypoints = {1, 0, 2, 2, 0};
+
+        int npoints = xpoints.length;
+        Path2D path = ConvexHull.getHull(xpoints, ypoints, npoints);
+        Vector2D[] vertices = pathVertices(path);
+
+        Vector2D[] expected = {new Vector2D(0, 0),
+                new Vector2D(2, 0),
+                new Vector2D(2, 2),
+                new Vector2D(0, 2)};
+
+        for (int i = 0; i < expected.length; i++) {
+            assertVector2DEquals(vertices[i], expected[i], TOLERANCE);
+        }
+    }
+
+    @Test
+    public void double2DArray()
+    {
+
+        double[][] arr ={{1.0, 1.0},{0.0, 0.0}, {2.0, 2.0}, {0.0, 2.0}, {2, 0.0}};
+        Path2D path = ConvexHull.getHull(arr);
+
+        Vector2D[] vertices = pathVertices(path);
+
+        Vector2D[] expected = {new Vector2D(0, 0),
+                new Vector2D(2, 0),
+                new Vector2D(2, 2),
+                new Vector2D(0, 2)};
+
+        for (int i = 0; i < expected.length; i++) {
+            assertVector2DEquals(vertices[i], expected[i], TOLERANCE);
+        }
+    }
+
+    @Test
+    public void int2DArray()
+    {
+        double[][] arr ={{1, 1},{0, 0}, {2, 2}, {0, 2}, {2, 0}};
+        Path2D path = ConvexHull.getHull(arr);
+
+        Vector2D[] vertices = pathVertices(path);
+
+        Vector2D[] expected = {new Vector2D(0, 0),
+                new Vector2D(2, 0),
+                new Vector2D(2, 2),
+                new Vector2D(0, 2)};
+
+        for (int i = 0; i < expected.length; i++) {
+            assertVector2DEquals(vertices[i], expected[i], TOLERANCE);
+        }
+    }
+
+    @Test
+    public void polyTest()
+    {
+        Polygon p = new Polygon();
+        p.addPoint(1, 1);
+        p.addPoint(0, 0);
+        p.addPoint(2, 2);
+        p.addPoint(0, 2);
+        p.addPoint(2, 0);
+
+        Path2D path = ConvexHull.getHull(p);
+
+        Vector2D[] vertices = pathVertices(path);
+
+        Vector2D[] expected = {new Vector2D(0, 0),
+                new Vector2D(2, 0),
+                new Vector2D(2, 2),
+                new Vector2D(0, 2)};
+
+        for (int i = 0; i < expected.length; i++) {
+            assertVector2DEquals(vertices[i], expected[i], TOLERANCE);
+        }
+    }
 }
